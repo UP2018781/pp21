470a75c03c51e6f9dcf4626d7f52ce87160fb140
up2018781 <91746177+UP2018781@users.noreply.github.com>
2021-11-26 14:51:15 +0000

    solved error

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
 const express = require('express');
-<<<<<<< HEAD
-const app = express();
-
-app.get('/', function (req, res) {
-    res.send('Hello World');
-})
-
-module.exports.app = app.listen(8080);
-=======
 const imager = require('./imager/imager.js');
-const imageLocation = 'resources';
 
 async function imgResponder(req,res){
     const width = req.params.width;
     const height = req.params.height;
     const square = req.query.square;
     const text = req.query.text;
-    console.log(`${width} x ${height}; square: ${square}, text: ${text}`);
-    // imager.sendImage(res,width,height,square,text);
-    res.end();
 
+    try {
+        height = parseInt(height);
+        width = parseInt(width);
+        square = parseInt(square);
+        text = toString(text);
+    } catch (error) {
+        res.status(400);
+    }
+
+    console.log(`${width} x ${height}; square: ${square}, text: ${text}`);
+    if (res.status != 400){
+        imager.sendImage(res,width,height,square,text);
+    }
+    res.end();
 }
 
 const app = express();
 
 app.use(express.static('public'));
 app.get('/img/:width/:height',imgResponder);
-app.get('/stats');
+// app.get('/stats');
 
 app.listen(8080);
->>>>>>> e4896ad30006fd782ccd55e1a51b7bb968983c3a
